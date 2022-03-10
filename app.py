546b3cb78ae428ee47c1bad43cd82e30b1210262
ilmarcio <45770403+ilmarcio@users.noreply.github.com>
2022-03-10 11:36:55 +0100

    Update and rename sol.py to app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def index():
 
 @app.route('/watermark', methods=['POST'])
 def apply_watermark():
-    bucket_name = "cgc-lab3"
+    bucket_name = "cgc3"
 
     filename = request.form['filename']
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
